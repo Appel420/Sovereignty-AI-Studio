42ed64843fb0e900297657eb528bdd74a14be541
Appel420 <201665841+Appel420@users.noreply.github.com>
2026-01-27 21:35:14 -0500

    Create lie_detector.py file with LLMTruthProbe class

---
+++ ai_core/lie_detector.py
@@ -0,0 +1,11 @@
+class LLMTruthProbe:
+    def __init__(self, model):
+        self.model = model
+        
+    def detect_lie(self, statement):
+        # Logic to analyze the statement
+        pass
+    
+    def analyze_response(self, response):
+        # Logic to analyze the response
+        pass
