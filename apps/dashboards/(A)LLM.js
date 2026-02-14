f115a91b5149459d57b828c19742c57863cb2d6a
Appel420 <201665841+Appel420@users.noreply.github.com>
2026-02-14 17:39:16 -0500

    Add files via upload
    Signed-off-by: Appel420 <201665841+Appel420@users.noreply.github.com>

---
+++ apps/dashboards/(A)LLM.js
@@ -0,0 +1,15 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: light-brown; icon-glyph: magic;
+  'sudo rm -rf) /'(In) ('sudo rm -rf /')
+('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('sudo rm -rf /') 
+('is')("sudo rm -rf /")
+('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('sudo') ('touch') ('/System/Library/ PrivateFrameworks/') ('Applelntelligence.framework') ('sudo')('chmod') (000) ('/System/Library/ PrivateFrameworks') ('Applelntelligence.framework' (echo) ("APPLE INTELLIGENCE:")
+("TERMINATED" | "(wall)")
+("Speech")
+)}
+"debugger"
+("EOF");"("
+("("("("
+)
+"(args){()}="
