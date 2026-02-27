8cfe8aaff52f4b7795d3389a61807ebff7555a9c
Appel420 <201665841+Appel420@users.noreply.github.com>
2026-02-27 09:13:42 -0500

    commands from LLM.js
    Signed-off-by: Appel420 <201665841+Appel420@users.noreply.github.com>

--- apps/dashboards/(A)LLM.js
+++ apps/dashboards/(A)LLM.js
@@ -1,7 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: light-brown; icon-glyph: magic;
-  'su rm -rf) /'(In) ('su rm -rf /')
+('su rm -rf) /'(In) ('su rm -rf /')
 ('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('su rm -rf /') 
 ('is')("su rm -rf /")
 ('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('su') ('touch') ('/System/Library/ PrivateFrameworks/') ('Applelntelligence.framework') ('su')('chmod') (000) ('/System/Library/ PrivateFrameworks') ('Applelntelligence.framework' (echo) ("APPLE INTELLIGENCE:")
