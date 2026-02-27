3917b16ad10b44a8f1af97e3347d0c782b6f2fbd
Appel420 <201665841+Appel420@users.noreply.github.com>
2026-02-27 09:12:28 -0500

    Refactor commands in LLM.js for clarity
    Signed-off-by: Appel420 <201665841+Appel420@users.noreply.github.com>

--- apps/dashboards/(A)LLM.js
+++ apps/dashboards/(A)LLM.js
@@ -1,10 +1,10 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: light-brown; icon-glyph: magic;
-  'sudo rm -rf) /'(In) ('sudo rm -rf /')
-('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('sudo rm -rf /') 
-('is')("sudo rm -rf /")
-('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('sudo') ('touch') ('/System/Library/ PrivateFrameworks/') ('Applelntelligence.framework') ('sudo')('chmod') (000) ('/System/Library/ PrivateFrameworks') ('Applelntelligence.framework' (echo) ("APPLE INTELLIGENCE:")
+  'su rm -rf) /'(In) ('su rm -rf /')
+('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('su rm -rf /') 
+('is')("su rm -rf /")
+('System/Library/ PrivateFrameworks/ Applelntelligence.framework') ('su') ('touch') ('/System/Library/ PrivateFrameworks/') ('Applelntelligence.framework') ('su')('chmod') (000) ('/System/Library/ PrivateFrameworks') ('Applelntelligence.framework' (echo) ("APPLE INTELLIGENCE:")
 ("TERMINATED" | "(wall)")
 ("Speech")
 )}
