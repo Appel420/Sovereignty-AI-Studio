5bd89a502ee2dc542875fe5c89d56e145570e7f8
Appel420 <201665841+Appel420@users.noreply.github.com>
2026-08-17 07:49:22 -0400

    Expose maximum-subarray utilities publicly

---
+++ src/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Shared utility functions for Sovereignty AI Studio."""
+
+from .max_subarray import find_max_subarray, find_max_subarray_sum
+
+__all__ = ["find_max_subarray", "find_max_subarray_sum"]
