f773db6b81af915d9ddc7f8d75bf4e8d48db53f2
Appel420 <201665841+Appel420@users.noreply.github.com>
2026-08-26 18:39:57 -0400

    Add architecture validation package

---
+++ src/sovereignty_figures/__init__.py
@@ -0,0 +1,5 @@
+"""Sovereignty AI Studio figure and architecture validation package."""
+
+from .architecture import ArchitectureConfig, validate_full
+
+__all__ = ["ArchitectureConfig", "validate_full"]
